5fa656e8ff6f9fbe754de522f071c52f77e80c3a
kodaimura <kodaimura123@gmail.com>
2024-10-12 23:33:32 +0900

    index page

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,21 +1,161 @@
 import { api } from '/js/api.js';
+import { getJaTime } from '/js/script.js';
 
-window.addEventListener("DOMContentLoaded", function() {
+let holidayCache = {};
+let targetDate;
+
+window.addEventListener("DOMContentLoaded", async () => {
+    targetDate = new Date(getJaTime().getFullYear(), getJaTime().getMonth() + 1);
     getProfile();
 
+    await fetchHolidays();
+    renderCalendar();
+    renderYearMonth();
+    renderLinks();
+    getShiftPreferred();
+    
+    document.getElementById('prev-month').addEventListener('click', handlePrevMonth);
+    document.getElementById('next-month').addEventListener('click', handleNextMonth);
     document.getElementById("shift-preferred").addEventListener("click", confirmProfileRegistered);
 });
+
+const fetchHolidays = async () => {
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
+    const url = `https://api.national-holidays.jp/${year}${String(month).padStart(2, '0')}`;
+    try {
+        holidayCache[`${year}-${month}`] = [];
+        const response = await fetch(url);
+        if (response.ok) {
+            holidayCache[`${year}-${month}`] = await response.json();
+        }
+    } catch (error) {
+        console.error('Error fetching holidays:', error);
+    }
+};
+
+const isHoliday = (year, month, day) => {
+    if (!holidayCache[`${year}-${month}`]) {
+        console.error(`祝日データがロードされていません: ${year}-${month}`);
+        return false;
+    }
+    return holidayCache[`${year}-${month}`].some((holiday) => {
+        return holiday.date === `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+    });
+};
 
 const getProfile = async () => {
     try {
         const result = await api.get('account_profile');
-        const form = document.getElementById("profile-form");
         document.getElementById('display_name').textContent = result.display_name;
         document.getElementById('account_role').textContent = result.account_role;
     } catch (e) {
         console.error(e)
     }
 }
+
+const renderCalendar = async () => {
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
+
+    const calendarBody = document.querySelector('#calendar tbody');
+    calendarBody.innerHTML = '';
+
+    const firstDay = new Date(year, month - 1, 1);
+    const lastDay = new Date(year, month, 0);
+
+    let row = document.createElement('tr');
+    for (let i = 0; i < firstDay.getDay(); i++) {
+        row.appendChild(document.createElement('td'));
+    }
+
+    for (let day = 1; day <= lastDay.getDate(); day++) {
+        const cell = document.createElement('td');
+        cell.textContent = day;
+
+        const dayOfWeek = new Date(year, month - 1, day).getDay();
+        if (dayOfWeek === 0 || isHoliday(year, month, day)) {
+            cell.classList.add('holiday');
+        } else if (dayOfWeek === 6) {
+            cell.classList.add('saturday');
+        } else {
+            cell.classList.add('weekday');
+        }
+
+        cell.dataset.day = day;
+
+        row.appendChild(cell);
+        if ((firstDay.getDay() + day) % 7 === 0) {
+            calendarBody.appendChild(row);
+            row = document.createElement('tr');
+        }
+    }
+
+    if (row.children.length > 0) {
+        calendarBody.appendChild(row);
+    }
+};
+
+const getShiftPreferred = async () => {
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
+    try {
+        const result = await api.get(`shift_preferred?year=${year}&month=${month}`);
+        for (let data of result) {
+            const accountId = data.account_id;
+            const dates = data.dates.split(',').map(Number);
+            for (let date of dates) {
+                const cell = document.querySelector(`td[data-day='${date}']`);
+                if (cell) {
+                    if (!cell.classList.contains('highlight')) {
+                        cell.classList.add('highlight');
+                    }
+                    cell.innerHTML += `&nbsp;${accountId}&nbsp;`;
+                }
+            };
+        }
+        result.forEach(date => {
+            
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+ const renderYearMonth = () => {
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
+
+    document.getElementById('year').textContent = year;
+    document.getElementById('month').textContent = month
+ };
+
+const renderLinks = () => {
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
+
+    document.getElementById('shift-preferred').href = `/shift_preferred/${year}/${month}`
+    document.getElementById('shift-generate').href = `/shift/${year}/${month}/generate`
+    document.getElementById('shift-edit').href = `/shift/${year}/${month}/edit`
+};
+
+const handlePrevMonth = async () => {
+    targetDate.setMonth(targetDate.getMonth() - 1);
+    await fetchHolidays();
+    renderCalendar();
+    renderYearMonth();
+    renderLinks();
+    getShiftPreferred();
+};
+
+const handleNextMonth = async () => {
+    targetDate.setMonth(targetDate.getMonth() + 1);
+    await fetchHolidays();
+    renderCalendar();
+    renderYearMonth();
+    renderLinks();
+    getShiftPreferred();
+};
 
 const confirmProfileRegistered = (event) => {
     if (
