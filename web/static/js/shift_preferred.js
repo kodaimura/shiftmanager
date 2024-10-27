84ed709accccc8ab372e669fc68d773d42571883
kodaimura <kodaimura123@gmail.com>
2024-10-27 18:27:45 +0900

    minor fix

--- web/static/js/shift_preferred.js
+++ web/static/js/shift_preferred.js
@@ -6,10 +6,9 @@ window.addEventListener("DOMContentLoaded", async () => {
     const year = parseInt(document.getElementById('year').value);
     const month = parseInt(document.getElementById('month').value); 
     await fetchHolidays(year, month);
-    await getShiftPreferred(year, month);
-    
     renderCalendar(year, month);
-    highlightSelectedDays();
+    await getShiftPreferreds(year, month);
+    await getShiftPreferred(year, month);
     document.getElementById("save").addEventListener("click", save);
 });
 
@@ -50,7 +49,11 @@ const renderCalendar = (year, month) => {
 
     for (let day = 1; day <= lastDay.getDate(); day++) {
         const cell = document.createElement('td');
-        cell.textContent = day;
+        const wrap = document.createElement('div');
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        div1.classList.add('day');
+        div2.classList.add('nums');
 
         const dayOfWeek = new Date(year, month - 1, day).getDay();
         if (dayOfWeek === 0 || isHoliday(year, month, day)) {
@@ -62,7 +65,13 @@ const renderCalendar = (year, month) => {
         }
 
         cell.dataset.day = day;
+        div1.textContent = day;
+        div2.dataset.day = day;
         cell.addEventListener('click', () => handleClickCell(cell, day));
+
+        wrap.appendChild(div1);
+        wrap.appendChild(div2);
+        cell.appendChild(wrap);
 
         row.appendChild(cell);
         if ((firstDay.getDay() + day) % 7 === 0) {
@@ -74,21 +83,6 @@ const renderCalendar = (year, month) => {
     if (row.children.length > 0) {
         calendarBody.appendChild(row);
     }
-};
-
-const highlightSelectedDays = () => {
-    const form = document.getElementById('shift-preferred-form');
-    const selectedDays = form.elements['dates'].value.split(',');
-
-    const cells = document.querySelectorAll('#calendar tbody td');
-    cells.forEach(cell => {
-        const day = cell.dataset.day;
-        if (selectedDays.includes(day)) {
-            cell.style.backgroundColor = 'yellow';
-        } else {
-            cell.style.backgroundColor = '';
-        }
-    });
 };
 
 const handleClickCell = (cell, day) => {
@@ -104,6 +98,23 @@ const handleClickCell = (cell, day) => {
     form.elements['dates'].value = selectedDays.join(',');
 };
 
+const getShiftPreferreds = async (year, month) => {
+    try {
+        const result = await api.get(`shift_preferreds?year=${year}&month=${month}`);
+        for (let data of result) {
+            const dates = data.dates.split(',').map(Number);
+            for (let date of dates) {
+                const cell = document.querySelector(`#calendar div[data-day='${date}']`);
+                if (cell) {
+                    cell.textContent = (parseInt(cell.textContent) || 0) + 1;
+                }
+            };
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 const getShiftPreferred = async (year, month) => {
     try {
         const response = await api.get(`shift_preferreds/me/${year}/${month}`);
@@ -111,6 +122,17 @@ const getShiftPreferred = async (year, month) => {
         if (dates) {
             const form = document.getElementById('shift-preferred-form');
             form.elements['dates'].value = dates;
+
+            const selectedDays = dates.split(',');
+            const cells = document.querySelectorAll('#calendar tbody td');
+            cells.forEach(cell => {
+                const day = cell.dataset.day;
+                if (selectedDays.includes(day)) {
+                    cell.style.backgroundColor = 'yellow';
+                } else {
+                    cell.style.backgroundColor = '';
+                }
+            });
         }
     } catch (e) {
         console.error(e);
@@ -125,7 +147,6 @@ const save = async () => {
         dates: form.elements['dates'].value,
         notes: '',
     };
-    alert(form.elements['dates'].value)
     try {
        await api.post(`shift_preferreds/me/${year}/${month}`, body);
        window.location.replace('/');
