f87a859c0e1570730916cf5220516e026490dddd
kodaimura <kodaimura123@gmail.com>
2025-09-20 08:53:43 +0900

    bug fix

--- internal/core/mail/mail.go
+++ internal/core/mail/mail.go
@@ -2,10 +2,10 @@ package mail
 
 import (
 	"fmt"
-	"strings"
-    "net/smtp"
 	"mime"
-	
+	"net/smtp"
+	"strings"
+
 	"shiftmanager/config"
 )
 
@@ -19,7 +19,7 @@ func Send (from string, to []string, subject, body string) error {
 
 func getSmtpAuth () smtp.Auth {
 	cf := config.GetConfig()
-	return smtp.PlainAuth("", cf.MailAccount, cf.MailPass, cf.MailHost)
+	return smtp.PlainAuth("", cf.MailUser, cf.MailPass, cf.MailHost)
 }
 
 func getSmtpAddress () string {
